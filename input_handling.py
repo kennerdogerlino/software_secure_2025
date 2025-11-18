1cbc433cf4dd9603f76fc29f0655834ff74f7a63
kennerdogerlino <ken.quisquino@education.nsw.gov.au>
2025-11-18 00:54:55 +0000

    sanitising user input, replacing user_name with hand_sanitiser_name where appropriate

--- input_handling.py
+++ input_handling.py
@@ -2,7 +2,7 @@ import re
 MAX_INPUT_LENGTH = 20
 
 def sanitise_input(name):
-    return re.sub(r'[^a-zA-Z0-9_]', '', name)
+    return re.sub(r'[^a-zA-Z\s-]', '', name).strip()
 
 #Get an integer between 1 & 10 input from user
 while True:
@@ -18,13 +18,14 @@ while True:
 while True:
     try:
         user_name = input('Pls enter your username: ')
-        if len(user_name) > MAX_INPUT_LENGTH:
+        hand_sanitiser_name = sanitise_input(user_name)
+        if len(hand_sanitiser_name) > MAX_INPUT_LENGTH:
             print(f'Username tuu long. Exceeds the max length of {MAX_INPUT_LENGTH} characters.')
-        elif not user_name.strip():
+        elif not hand_sanitiser_name.strip():
             print('Username cannot be blank or whitespace')
         break
     except ValueError:
         print('Pls input a valid string.')
 
 print(f'Valid number {num}')
-print(f'Valid user_name {user_name}')
+print(f'Valid user_name {hand_sanitiser_name}')
